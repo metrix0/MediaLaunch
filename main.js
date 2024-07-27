6eb6c127dd5801d446b3e89bb4611a1980a5882b
J. Almeida (Madoka) <34656666+metrix0@users.noreply.github.com>
2024-07-27 13:53:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,131 @@
+function Welcome(){
+
+    let time = 1
+    setTimeout(function (){
+        removeLoader()
+        document.title = "Media Launch"
+    },time)
+
+    setTimeout(function (){
+        testimonialsUl.innerHTML = ''
+        testimoialsInsert()
+    },1)
+
+}
+function removeLoader(){
+
+    setTimeout(()=>{
+            let loader = document.getElementById('loader');
+
+            // hide the loader
+            loader.style = 'display: none;';
+        },
+        1);
+}
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
+
+function shiver(bool, click, el) {
+    const arrows = document.getElementsByClassName('flying')
+
+    if(bool){
+        for(let i = 0; i <arrows.length; i++){
+            arrows[i].classList.add('shiver')
+        }
+    }
+    else{
+        for(let i = 0; i <arrows.length; i++){
+            arrows[i].classList.remove('shiver')
+        }
+        if(click){
+            for(let i = 0; i <arrows.length; i++){
+                arrows[i].classList.add('burst')
+            }
+            el.onmouseover = function (){}
+            setTimeout(function (){
+                el.onmouseover = function (){shiver(true)}
+            },1500)
+        }
+    }
+}
+
+function passOver(){
+    const cont = document.getElementById('passOver')
+    cont.classList.remove('none')
+    cont.style.left = '0%'
+    setTimeout(function (){window.open('a')},1000)
+    setTimeout(function (){
+        cont.style.left = '200%'
+        document.getElementById('arrows').innerHTML = '<i class="fa-solid fa-arrow-right flying" style="animation-delay: 1s;font-size: 480%;left: -3%; position: absolute; z-index: 1; transform: scaleX(110%); color: var(--select2)"></i>\n' +
+            '        <i class="fa-solid fa-arrow-right flying" style="top: -20%;right: 0; font-size: 350%; position: absolute; z-index: 1; transform: scaleX(110%); color: var(--select2)"></i>\n' +
+            '        <i class="fa-solid fa-arrow-right flying" style="animation-delay: 2s;left: 60%;bottom: 5%;font-size: 550%; position: absolute; z-index: 1; transform: scaleX(110%); color: var(--select2)"></i>\n'
+    },2000)
+}
+
+const testimonials = [
+    {
+        "name":"Cowbrownies",
+        "imgpath":"cowbrownies.png",
+        "result":"+1000 VENDAS/MÊS",
+        "desc": "Cowbrownies precisava de um visual belo e que dá muita fome!"
+    },
+    {
+        "name":"John Vitor",
+        "imgpath":"johnvitor.png",
+        "result":"+R$ 5.000/MÊS FATURAMENTO",
+        "desc": "Entregamos um visual profissional que atrai o tipo certo de cliente."
+    },
+    {
+        "name":"Baliero",
+        "imgpath":"baliero.png",
+        "result":"+ RENOME E TRÁFEGO",
+        "desc": "Combinamos nossa criatividade com suas fotos incríveis."
+    },
+]
+const testimonialsUl = document.getElementById('testimoinalGrid')
+var shadowIndex = 1
+function testimoialsInsert(){
+    for(let i = 0; i < testimonials.length; i++){
+        const div = document.createElement('div')
+        div.classList.add('gridElement')
+        div.classList.add('shadow'+shadowIndex)
+        if(shadowIndex++ >= 3){shadowIndex = 1}
+        div.innerHTML = '        <img src="img/testimonials/'+testimonials[i].imgpath+'" style="width: 100%; border-radius: 4px 4px 0 0">' +
+            '        <p style="color: white;background: linear-gradient(10deg, var(--grad1) 0%, var(--grad2) 50%, var(--grad3) 100%);padding-bottom: 5%; border-radius: 0 0 4px 4px;padding-top: 3%; text-align: center">' +
+            '          <span class="cand">' +
+            '            '+testimonials[i].result+'' +
+            '          </span>' +
+            '          <br>' +
+            '          <span style="font-size: 85%; width: 90%; display: inline-block;margin-top: 2%">' +
+            '            '+testimonials[i].desc+'' +
+            '          </span>' +
+            '          <br>' +
+            '          <span style="font-size: 60%;margin-top: 5%;display: inline-block;cursor: pointer;" onclick="window.open(\'wpp\')">' +
+            '            Saiba mais' +
+            '          </span>' +
+            '        </p>'
+        testimonialsUl.appendChild(div)
+    }
+}
+
+function openMenu(op){
+    const cfg = document.getElementById('menu')
+    if(op){
+        cfg.classList.remove('none')
+        setTimeout(function (){cfg.classList.remove('hiddenMenu')},10)
+    }
+    else{
+        cfg.classList.add('hiddenMenu')
+        setTimeout(function (){cfg.classList.add('none')},800)
+    }
+}
